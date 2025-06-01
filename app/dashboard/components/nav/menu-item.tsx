d41a48a3124336d2550e524ea3c0594cbbb4bc67
Robin <philoin88@gmail.com>
2025-06-02 00:08:56 +0900

    feat: Add Mobile Menu

--- app/dashboard/components/nav/menu-item.tsx
+++ app/dashboard/components/nav/menu-item.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import { DrawerContext } from '@/components/ui/drawer';
 import { cn } from '@/lib/utils';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useContext } from 'react';
 
 type Props = {
   children?: React.ReactNode;
@@ -12,6 +14,7 @@ type Props = {
 export default function MenuItem({ children, href }: Props) {
   const pathname = usePathname();
   const isActive = pathname === href;
+  const { onClose } = useContext(DrawerContext);
 
   return (
     <Link
@@ -21,6 +24,7 @@ export default function MenuItem({ children, href }: Props) {
           'bg-primary hover:bg-primary dark:hover:bg-primary hover:text-primary-foreground text-primary-foreground'
       )}
       href={href}
+      onClick={onClose}
     >
       {children}
     </Link>
